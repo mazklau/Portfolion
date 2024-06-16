55184d8b627a5905534430ca8b837f97b6c22564
Matheus Guilherme Sousa Bezerra <84602548+mazklau@users.noreply.github.com>
2024-06-16 11:14:39 -0300

    Atualizei

---
+++ js/animacao.js
@@ -0,0 +1,11 @@
+<div class="cube">
+<div class="face face--front"><i class='bx bxl-css3' ></i></div>
+<div class="face face--right"></div>
+<div class="face face--back"><i class='bx bxl-css3' ></i></div>
+<div class="face face--left"></div>
+<div class="face face--top"></div>
+<div class="face face--bottom"></div>
+</div> 
+
+
+
