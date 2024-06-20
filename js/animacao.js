175cf356e9923824f2c98cac8655654f2d0916b8
Matheus Guilherme Sousa Bezerra <84602548+mazklau@users.noreply.github.com>
2024-06-20 19:56:06 -0300

    Update animacao.js

--- js/animacao.js
+++ js/animacao.js
@@ -1,26 +1,12 @@
-function toggleMenu() {
-    var content = document.getElementById("content");
-    if (content.style.display === "none") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
-
 function changeIcon(icon) {
-    icon.classList.toggle("bx-menu");
-    icon.classList.toggle("bx-x");
-}
-
-// Chama a função changeIcon dentro de toggleMenu para alternar o ícone também
-function toggleMenu() {
-    var content = document.getElementById("content");
-    var icon = document.getElementById("toggleButton");
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-        changeIcon(icon);
-    } else {
-        content.style.display = "none";
-        changeIcon(icon);
-    }
-}
+            const menuItems = document.querySelector('.menu-items');
+            menuItems.classList.toggle('show');
+            
+            if (menuItems.classList.contains('show')) {
+                icon.classList.remove('bx-menu');
+                icon.classList.add('bx-x');
+            } else {
+                icon.classList.remove('bx-x');
+                icon.classList.add('bx-menu');
+            }
+        }
