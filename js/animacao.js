5372848d1671b837868fc5561458d2743608e239
Matheus Guilherme Sousa Bezerra <84602548+mazklau@users.noreply.github.com>
2024-06-20 19:11:45 -0300

    Update animacao.js

--- js/animacao.js
+++ js/animacao.js
@@ -10,4 +10,14 @@ function changeIcon(icon) {
                 icon.classList.remove('bx-x');
                 icon.classList.add('bx-menu');
             }
-        }
+        };
+
+const toggleButton = document.querySelector('.items');
+toggleButton.addEventListener('click', () => {
+    // Verifica o estado atual do display e alterna entre 'none' e 'block'
+    if (content.style.display === 'none') {
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
+});
